a0be8cd09d45106fbc36a9409bace86ef1424433
arrietta <39980503+arrietta@users.noreply.github.com>
2024-04-03 09:17:33 +0500

    icon added

--- Main/models.py
+++ Main/models.py
@@ -26,28 +26,34 @@ class Product(models.Model):
 
 class Portal(Product):
     image = models.ImageField(upload_to='Portal/', verbose_name='Изображение')
+    icon = models.ImageField(upload_to='icon/Portal/', verbose_name='Изображение')
 
 
 class Carnice(Product):
     image = models.ImageField(upload_to='Carnice/', verbose_name='Изображение')
+    icon = models.ImageField(upload_to='icon/Carnice/', verbose_name='Изображение')
 
 
 class Podium(Product):
     image = models.ImageField(upload_to='Podium/', verbose_name='Изображение')
+    icon = models.ImageField(upload_to='icon/Podium/', verbose_name='Изображение')
 
 
 class Socket(Product):
     image = models.ImageField(upload_to='Socket/', verbose_name='Изображение')
+    icon = models.ImageField(upload_to='icon/Socket/', verbose_name='Изображение')
 
 
 class Boots(Product):
     image = models.ImageField(upload_to='Boots/', verbose_name='Изображение')
+    icon = models.ImageField(upload_to='icon/Boots/', verbose_name='Изображение')
 
 
 class BaseDoor(models.Model):
     collection = models.ForeignKey(Collections, on_delete=models.CASCADE, index=True)
     door_type = models.CharField(max_length=127)
     door_image = models.ImageField(upload_to='Door/', verbose_name='Изображение')
+    icon = models.ImageField(upload_to='icon/Door/', verbose_name='Изображение')
     shape = models.CharField(max_length=127)
     shape_price = models.FloatField()
     portal = models.ForeignKey(Portal, on_delete=models.CASCADE)
@@ -72,16 +78,20 @@ class Doors(BaseDoor):
 
 class ClassicDoor(BaseDoor):
     Bevel = models.CharField(max_length=127)
+    icon = models.ImageField(upload_to='icon/Bevel/', verbose_name='Изображение')
 
 
 class ClassicBaguette(BaseDoor):
     Molding = models.CharField(max_length=127)
     Molding_image = models.ImageField(upload_to='Molding/', verbose_name='Изображение')
+    icon = models.ImageField(upload_to='icon/Molding/', verbose_name='Изображение')
 
 
 class GrigliaDoor(BaseDoor):
     Grid = models.CharField(max_length=127)
     Grid_image = models.ImageField(upload_to='Grid/', verbose_name='Изображение')
+    icon = models.ImageField(upload_to='icon/Grid/', verbose_name='Изображение')
+    Bevel_icon = models.ImageField(upload_to='icon/Bevel/', verbose_name='Изображение')
     Bevel = models.CharField(max_length=127)
 
 
