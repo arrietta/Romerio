8aa36e0029330c16b613bcf550ab26383132dcf7
arrietta <39980503+arrietta@users.noreply.github.com>
2024-04-13 12:49:49 +0500

    first commit

--- Main/models.py
+++ Main/models.py
@@ -13,89 +13,92 @@ class Collections(models.Model):
         ]
 
 
+class Shape(models.Model):
+    collections = models.ForeignKey(Collections, on_delete=models.CASCADE)
+    door_type = models.CharField(max_length=127)
+    name = models.CharField(max_length=127, primary_key=True)
+
+    price = models.FloatField()
+    image = models.ImageField(upload_to='Shape/', verbose_name='Photo')
+    icon = models.ImageField(upload_to='icon/Shape/', verbose_name='icon')
+    Grid_image = models.ImageField(upload_to='Grid/', verbose_name='Grid Photo', default="none.png")
+    Grid_icon = models.ImageField(upload_to='icon/Grid/', verbose_name='Grid Icon', default="none.png")
+    Bevel_icon = models.ImageField(upload_to='icon/Bevel/', verbose_name='Bevel icon', default="none.png")
+
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['collections'])
+        ]
+
+
 class Product(models.Model):
+    collections = models.ForeignKey(Collections, on_delete=models.CASCADE)
+    shape = models.ForeignKey(Shape, on_delete=models.CASCADE)
     name = models.CharField(max_length=127, primary_key=True)
     price = models.FloatField()
 
     class Meta:
         abstract = True
         indexes = [
-            models.Index(fields=['name'])
+            models.Index(fields=['collections'])
         ]
 
 
 class Portal(Product):
-    image = models.ImageField(upload_to='Portal/', verbose_name='Изображение')
-    icon = models.ImageField(upload_to='icon/Portal/', verbose_name='Изображение', default="none.png")
+    image = models.ImageField(upload_to='Portal/', verbose_name='Photo')
+    icon = models.ImageField(upload_to='icon/Portal/', verbose_name='icon')
+
+
+class Molding(Product):
+    image = models.ImageField(upload_to='Molding/', verbose_name='Photo')
+    icon = models.ImageField(upload_to='icon/Molding/', verbose_name='icon')
 
 
 class Carnice(Product):
-    image = models.ImageField(upload_to='Carnice/', verbose_name='Изображение')
-    icon = models.ImageField(upload_to='icon/Carnice/', verbose_name='Изображение', default="none.png")
+    image = models.ImageField(upload_to='Carnice/', verbose_name='Photo')
+    icon = models.ImageField(upload_to='icon/Carnice/', verbose_name='icon')
 
 
 class Podium(Product):
-    image = models.ImageField(upload_to='Podium/', verbose_name='Изображение')
-    icon = models.ImageField(upload_to='icon/Podium/', verbose_name='Изображение', default="none.png")
+    image = models.ImageField(upload_to='Podium/', verbose_name='Photo')
+    icon = models.ImageField(upload_to='icon/Podium/', verbose_name='icon')
 
 
 class Socket(Product):
-    image = models.ImageField(upload_to='Socket/', verbose_name='Изображение')
-    icon = models.ImageField(upload_to='icon/Socket/', verbose_name='Изображение', default="none.png")
+    image = models.ImageField(upload_to='Socket/', verbose_name='Photo')
+    icon = models.ImageField(upload_to='icon/Socket/', verbose_name='icon')
 
 
 class Boots(Product):
-    image = models.ImageField(upload_to='Boots/', verbose_name='Изображение')
-    icon = models.ImageField(upload_to='icon/Boots/', verbose_name='Изображение', default="none.png")
+    image = models.ImageField(upload_to='Boots/', verbose_name='Photo')
+    icon = models.ImageField(upload_to='icon/Boots/', verbose_name='icon')
 
 
-class BaseDoor(models.Model):
-    collection = models.ForeignKey(Collections, on_delete=models.CASCADE)
-    door_type = models.CharField(max_length=127)
-    door_image = models.ImageField(upload_to='Door/', verbose_name='Изображение')
-    icon = models.ImageField(upload_to='icon/Door/', verbose_name='Изображение', default="none.png")
-    shape = models.CharField(max_length=127)
-    shape_price = models.FloatField()
-    portal = models.ForeignKey(Portal, on_delete=models.CASCADE)
-    carnice = models.ForeignKey(Carnice, on_delete=models.CASCADE)
-    podium = models.ForeignKey(Podium, on_delete=models.CASCADE)
-    socket = models.ForeignKey(Socket, on_delete=models.CASCADE)
-    boots = models.ForeignKey(Boots, on_delete=models.CASCADE)
-    door_price = models.FloatField()
+# class BaseDoor(models.Model):
+#     collection = models.ForeignKey(Collections, on_delete=models.CASCADE)
+#     door_type = models.CharField(max_length=127)
+#     door_image = models.ImageField(upload_to='Door/', verbose_name='Изображение')
+#     icon = models.ImageField(upload_to='icon/Door/', verbose_name='Изображение', default="none.png")
+#     shape = models.CharField(max_length=127)
+#     shape_price = models.FloatField()
+#     portal = models.ForeignKey(Portal, on_delete=models.CASCADE)
+#     carnice = models.ForeignKey(Carnice, on_delete=models.CASCADE)
+#     podium = models.ForeignKey(Podium, on_delete=models.CASCADE)
+#     socket = models.ForeignKey(Socket, on_delete=models.CASCADE)
+#     boots = models.ForeignKey(Boots, on_delete=models.CASCADE)
+#     door_price = models.FloatField()
 
-    class Meta:
-        abstract = True
-        indexes = [
-            models.Index(fields=['collection'])
-        ]
+# class Meta:
+#     abstract = True
+#     indexes = [
+#         models.Index(fields=['collection'])
+#     ]
 
-    def save(self, *args, **kwargs):
-        self.door_price = (self.portal.price + self.carnice.price + self.podium.price +
-                           self.socket.price + self.boots.price + self.shape_price)
-        super().save(*args, **kwargs)
-
-
-class Doors(BaseDoor):
-    pass
-
-
-class ClassicDoor(BaseDoor):
-    Bevel = models.CharField(max_length=127)
-    icon = models.ImageField(upload_to='icon/Bevel/', verbose_name='Изображение', default="none.png")
-
-
-class ClassicBaguette(BaseDoor):
-    Molding = models.CharField(max_length=127)
-    Molding_image = models.ImageField(upload_to='Molding/', verbose_name='Изображение')
-    icon = models.ImageField(upload_to='icon/Molding/', verbose_name='Изображение', default="none.png")
-
-
-class GrigliaDoor(BaseDoor):
-    Grid = models.CharField(max_length=127)
-    Grid_image = models.ImageField(upload_to='Grid/', verbose_name='Изображение')
-    icon = models.ImageField(upload_to='icon/Grid/', verbose_name='Изображение', default="none.png")
-    Bevel_icon = models.ImageField(upload_to='icon/Bevel/', verbose_name='Изображение' , default="none.png")
-    Bevel = models.CharField(max_length=127)
+# def save(self, *args, **kwargs):
+#     self.door_price = (self.portal.price + self.carnice.price + self.podium.price +
+#                        self.socket.price + self.boots.price + self.shape_price)
+#     super().save(*args, **kwargs)
 
 
 class Cart(models.Model):
@@ -104,8 +107,12 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    base_door = models.ForeignKey(Doors, on_delete=models.CASCADE, null=True, blank=True)
-    classic_door = models.ForeignKey(ClassicDoor, on_delete=models.CASCADE, null=True, blank=True)
-    classic_baguette = models.ForeignKey(ClassicBaguette, on_delete=models.CASCADE, null=True, blank=True)
-    griglia_door = models.ForeignKey(GrigliaDoor, on_delete=models.CASCADE, null=True, blank=True)
+    shape = models.ForeignKey(Shape, on_delete=models.CASCADE)
+    molding = models.ForeignKey(Molding, on_delete=models.CASCADE)
+    portal = models.ForeignKey(Portal, on_delete=models.CASCADE)
+    carnice = models.ForeignKey(Carnice, on_delete=models.CASCADE)
+    podium = models.ForeignKey(Podium, on_delete=models.CASCADE)
+    socket = models.ForeignKey(Socket, on_delete=models.CASCADE)
+    boots = models.ForeignKey(Boots, on_delete=models.CASCADE)
+    door_price = models.FloatField()
     quantity = models.PositiveIntegerField(default=1)
