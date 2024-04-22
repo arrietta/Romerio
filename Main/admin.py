c8b7dd82cc140d6f0f45d8e084932b2b8cdd91d6
arrietta <39980503+arrietta@users.noreply.github.com>
2024-04-22 17:56:41 +0500

    shpon

--- Main/admin.py
+++ Main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Collections, Portal, Carnice, Podium, Socket, Boots, CartItem, Shape, Molding,Shpon_Boots,Shpon_Podium,Shpon_Socket,Shpon_Carnice,Shpone
+from .models import Collections, Portal, Carnice, Podium, Socket, Boots, CartItem, Shape, Molding,Shpon_Boots,Shpon_Socket,Shpon_Carnice,Shpone
 
 admin.site.register(Collections)
 admin.site.register(Shape)
