abd46051e820a5c23591402b583d5b6e3e914ec9
arrietta <39980503+arrietta@users.noreply.github.com>
2024-04-22 08:29:22 +0500

    first_front

--- Romerio/wsgi.py
+++ Romerio/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Romerio project.
+WSGI config for MyDoors project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,15 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Romerio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyDoors.settings')
 
+application = get_wsgi_application()
+
+
+# -*- coding: utf-8 -*-
+import os, sys
+sys.path.insert(0, '/var/www/u0000006/data/www/romerio.ru/Doors')
+sys.path.insert(1, '/var/www/u0000006/data/djangoenv/lib/python3.10/site-packages')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Doors.settings'
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
