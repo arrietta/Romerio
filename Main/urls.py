21536d80cb4a97c9431f932b94157dd77add64c4
arrietta <39980503+arrietta@users.noreply.github.com>
2024-04-22 22:17:59 +0500

    shpon

--- Main/urls.py
+++ Main/urls.py
@@ -4,11 +4,12 @@ from Romerio import settings
 from . import views
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('', views.main, name='main'),
-    path('catalog', views.catalog, name='catalog'),
-    path('cart/', views.cart, name='cart'),
-    path('constructor/<str:key>', views.constructor, name='constructor'),
-    path('api/', views.api, name='api'),
+urlpatterns = ([
+                  path('', views.main, name='main'),
+                  path('catalog', views.catalog, name='catalog'),
+                  path('cart/', views.cart, name='cart'),
+                  path('constructor/<str:key>', views.constructor, name='constructor'),
+                  path('api/', views.api, name='api'),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +
+               static(settings.STATIC_URL,document_root=settings.STATIC_ROOT))
