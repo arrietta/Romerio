75da3a057067845495be135e51c398d36fcf2fbd
arrietta <39980503+arrietta@users.noreply.github.com>
2024-04-25 22:21:55 +0500

    shpon

--- Romerio/wsgi.py
+++ Romerio/wsgi.py
@@ -19,7 +19,7 @@ application = get_wsgi_application()
 # -*- coding: utf-8 -*-
 import os, sys
 
-sys.path.insert(0, '/var/www/u0000006/data/www/romerio.ru/Doors')
+sys.path.insert(0, '/var/www/u0000006/data/www/romerio.ru/Romerio')
 sys.path.insert(1, '/var/www/u0000006/data/djangoenv/lib/python3.10/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'Romerio.settings'
 from django.core.wsgi import get_wsgi_application
