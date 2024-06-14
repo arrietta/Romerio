0fb7f085a54d2203b3bf50cbb0fc08f8382d05e5
arrietta <39980503+arrietta@users.noreply.github.com>
2024-06-14 23:42:20 +0500

    add

--- Main/telegram_bot.py
+++ Main/telegram_bot.py
@@ -3,12 +3,10 @@ from telebot import types
 
 from Romerio import settings
 
-# Замените 'YOUR_BOT_TOKEN' на токен вашего Telegram бота
 bot = telebot.TeleBot('6442182992:AAFF7xkljKsaHEmNh0PfE9k2rMXjZbGii0s')
-
+chat_id = '@My_Alpha_Doors'
 def send_message_to_bot(message):
     try:
-        chat_id = '@My_Alpha_Doors'
         bot.send_message(chat_id, message)
     except Exception as e:
         print(f"Произошла ошибка при отправке сообщения в Telegram: {str(e)}")
@@ -16,7 +14,6 @@ def send_message_to_bot(message):
 def send_photo_with_message_to_bot(link, message):
     try:
         print(f"{settings.BASE_DIR}{link}")
-        chat_id = '@My_Alpha_Doors'
         photo_path = f"{settings.BASE_DIR}{link}"
         photo_input = types.InputFile(photo_path)
         bot.send_photo(chat_id, photo_input, caption=message)
